340f20bf60ec732ad4846530bd9525cd26e655b7
Isaac Ure <iur11@uclive.ac.nz>
2025-03-28 12:05:14 +1300

    Testing display with minifb

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,49 @@
 // Thanks to: https://tobiasvl.github.io/blog/write-a-chip-8-emulator/
 
+use minifb::{Key, Window, WindowOptions, ScaleMode};
+
+const DISPLAY_WIDTH: usize = 64;
+const DISPLAY_HEIGHT: usize = 32;
+
 fn main() {
+    let mut display_buffer: Vec<u32> = vec![0; DISPLAY_WIDTH * DISPLAY_HEIGHT];
+
+    let mut window = Window::new(
+        "Test - ESC to exit",
+        DISPLAY_WIDTH,
+        DISPLAY_HEIGHT,
+        // WindowOptions::default())
+        // .unwrap_or_else(|e| {
+        //     panic!("{}", e);
+        WindowOptions {
+            scale_mode: ScaleMode::AspectRatioStretch,
+            resize: true,
+            ..WindowOptions::default()
+        }).unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+
+    window.set_target_fps(60);
+    let mut memory: [u8; 4096] = [0; 4096];
+    store_font(&mut memory);
+    
+    let mut counter = 0;
+    for i in display_buffer.iter_mut() {
+        *i = if memory[counter] == 1 {0xFFFFFF} else {0};
+        counter += 1;
+    }
+    
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        // Update the display buffer
+        window.update_with_buffer(&display_buffer, DISPLAY_WIDTH, DISPLAY_HEIGHT).unwrap();
+    }
+}
+
+fn draw_sprite(sprite: Vec<u8>, buffer: &mut Vec<u32>, position: u32) {
+    
+}
+
+fn setup() {
     let mut memory: [u8; 4096] = [0; 4096];
     let mut program_counter: u16 = 0x0;
     let mut index_register: u16 = 0x0;
@@ -8,8 +51,6 @@ fn main() {
     let mut delay_timer: u8 = 0;
     let mut sound_timer: u8 = 0;
     let mut registers: [u8; 16] = [0; 16];
-
-
 }
 
 fn store_font(memory: &mut [u8; 4096]) {
@@ -31,6 +72,6 @@ fn store_font(memory: &mut [u8; 4096]) {
     0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
     0xF0, 0x80, 0xF0, 0x80, 0x80]; // F
     
-    memory[0x0f0..0x050 + font.len()].copy_from_slice(&font);
+    memory[0x050..0x050 + font.len()].copy_from_slice(&font);
 }
 
