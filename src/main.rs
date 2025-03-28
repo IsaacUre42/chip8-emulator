ff010c7f0327422d9b4f7db8bb91e1eabb4f2f41
Isaac Ure <iur11@uclive.ac.nz>
2025-03-28 14:40:44 +1300

    Doing OO

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,23 @@ use minifb::{Key, Window, WindowOptions, ScaleMode};
 const DISPLAY_WIDTH: usize = 64;
 const DISPLAY_HEIGHT: usize = 32;
 
+struct Emulator {
+    memory: [u8; 4096],
+    program_counter: u16,
+    index_register: u16,
+    stack: Vec<u16>,
+    delay_timer: u8,
+    sound_timer: u8,
+    registers: [u8; 16]
+}
+
 fn main() {
-    let mut display_buffer: Vec<u32> = vec![0; DISPLAY_WIDTH * DISPLAY_HEIGHT];
+    let display_buffer: Vec<u32> = vec![0; DISPLAY_WIDTH * DISPLAY_HEIGHT];
 
     let mut window = Window::new(
         "Test - ESC to exit",
         DISPLAY_WIDTH,
         DISPLAY_HEIGHT,
-        // WindowOptions::default())
-        // .unwrap_or_else(|e| {
-        //     panic!("{}", e);
         WindowOptions {
             scale_mode: ScaleMode::AspectRatioStretch,
             resize: true,
@@ -27,20 +34,23 @@ fn main() {
     let mut memory: [u8; 4096] = [0; 4096];
     store_font(&mut memory);
     
-    let mut counter = 0;
-    for i in display_buffer.iter_mut() {
-        *i = if memory[counter] == 1 {0xFFFFFF} else {0};
-        counter += 1;
-    }
-    
     while window.is_open() && !window.is_key_down(Key::Escape) {
         // Update the display buffer
         window.update_with_buffer(&display_buffer, DISPLAY_WIDTH, DISPLAY_HEIGHT).unwrap();
     }
 }
 
-fn draw_sprite(sprite: Vec<u8>, buffer: &mut Vec<u32>, position: u32) {
-    
+fn draw_sprite(sprite: &Vec<u8>, buffer: &mut Vec<u32>, position: u32) {
+    //max sprite size is 8 by 15 pxls
+    let mut y = 0;
+    for byte in sprite {
+        for offset in 0..8 {
+            let index = (position + offset) as usize + (y * DISPLAY_WIDTH);
+            let bit = (byte >> (7 - offset)) & 1;
+            buffer[index] = if bit == 1 {0x00FF00} else {0}
+        }
+        y += 1;
+    }
 }
 
 fn setup() {
